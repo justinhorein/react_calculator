9b1a5ad91d040b19511773e3d70f143953aa251f
Justin Horein <justinhorein@gmail.com>
2022-08-23 17:41:52 -0700

    ui looks good

--- src/App.js
+++ src/App.js
@@ -4,20 +4,38 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="calculator">
+        <div className="screen">
+        
+        </div>
+        <div className="buttons">
+          <div className="column">
+            <button>+</button>
+            <button>7</button>
+            <button>4</button>
+            <button>1</button>
+            <button>0</button>
+          </div>
+          <div className="column">
+            <button>-</button>
+            <button>8</button>
+            <button>5</button>
+            <button>2</button>
+            <button>.</button>
+          </div>
+          <div className="column">
+            <button>x</button>
+            <button>9</button>
+            <button>6</button>
+            <button>3</button>
+            <button>AC</button>
+          </div>
+          <div className="column-last">
+            <button>÷</button>
+            <button>=</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
