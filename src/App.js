62792ba68490c7858844d700423952e535ee58bb
Justin Horein <justinhorein@gmail.com>
2022-08-25 20:45:56 -0700

    basic functionality working!

--- src/App.js
+++ src/App.js
@@ -16,8 +16,12 @@ function Calculator() {
 
   function handleNum(num) {
     let decimals = screen.split(".").length - 1;
+    let len = screen.length;
 
-    if (decimals == "1" && num == ".") {
+    if (len > 16) {
+      // do nothing
+    }
+    else if (decimals == "1" && num == "." && screen != total) {
       // do nothing
     }
     else if (screen == "0" && num == ".") {
@@ -30,7 +34,12 @@ function Calculator() {
       setScreen(screen + num);
     }
     else if (screen == total) {
-      setScreen(num);
+      if (num == ".") {
+        setScreen("0.")
+      }
+      else {
+        setScreen(num);
+      }
     }
     else {
       setScreen(screen + num);
@@ -39,11 +48,13 @@ function Calculator() {
 
   function handleOperator(op) {
     if (op == "AC") {
+      setTotal("")
       setScreen("0");
       setFirstNum("");
       setOperator("");
     }
     else if (firstNum == "") {
+      setTotal(screen);
       setFirstNum(screen);
       setScreen("0");
       setOperator(op);
@@ -53,66 +64,56 @@ function Calculator() {
         let tot;
         if (operator == "+") {
           tot = (parseFloat(firstNum) + parseFloat(screen)).toString();
-          setTotal(tot);
-          setScreen(tot);
-          setOperator("");
         }
         else if (operator == "-") {
           tot = (parseFloat(firstNum) - parseFloat(screen)).toString();
-          setTotal(tot);
-          setScreen(tot);
-          setOperator("");
+        }
+        else if (operator == "x") {
+          tot = (parseFloat(firstNum) * parseFloat(screen)).toString();
+        }
+        else if (operator == "÷") {
+          tot = (parseFloat(firstNum) / parseFloat(screen)).toString();
         }
 
+        if (tot.length > 16) {
+          tot = tot.substring(0, 16);
+        }
+
+        setTotal(tot);
+        setScreen(tot);
         setFirstNum(tot);
+        setOperator("");
       }
       else {
         //  If equal sign was previously used
         if (operator == "") {
           setOperator(op);
         }
-        else if (operator == "+") {
-          let tot = (parseFloat(firstNum) + parseFloat(screen)).toString();
-          setTotal(tot);
-          setScreen(tot);
-          setOperator(op);
-          setFirstNum(tot);
-        }
-        else if (operator == "-") {
-          let tot = (parseFloat(firstNum) - parseFloat(screen)).toString();
-          setTotal(tot);
-          setScreen(tot);
-          setOperator(op);
-          setFirstNum(tot);
-        }
-        else if (operator == "x") {
-          let tot = (parseFloat(firstNum) * parseFloat(screen)).toString();
-          setTotal(tot);
-          setScreen(tot);
-          setOperator(op);
-          setFirstNum(tot);
-        }
-        else if (operator == "÷") {
-          let tot = (parseFloat(firstNum) / parseFloat(screen)).toString();
-          setTotal(tot);
-          setScreen(tot);
-          setOperator(op);
-          setFirstNum(tot);
-        }
-      }
-      // else if (op == "-") {
-      //   if (operator == "") {
-      //       setOperator(op);
-      //   }
-      //   else {
-      //     let tot = parseFloat(firstNum) - parseFloat(screen);
-      //     tot = tot.toString();
-      //     setScreen(tot);
-      //     setOperator("-");
-      //     setFirstNum(tot);
-      //   }
-      // }
+        else {
+          let tot;
+          if (operator == "+") {
+            tot = (parseFloat(firstNum) + parseFloat(screen)).toString();
+          }
+          else if (operator == "-") {
+            tot = (parseFloat(firstNum) - parseFloat(screen)).toString();
+          }
+          else if (operator == "x") {
+            tot = (parseFloat(firstNum) * parseFloat(screen)).toString();
+          }
+          else if (operator == "÷") {
+            tot = (parseFloat(firstNum) / parseFloat(screen)).toString();
+          }
 
+          if (tot.length > 16) {
+            tot = tot.substring(0, 16);
+          }
+
+          setTotal(tot);
+          setScreen(tot);
+          setOperator(op);
+          setFirstNum(tot);
+        }     
+      }
     }
   }
 
